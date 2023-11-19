f5b1aab0aa1f4812179e171a16a87c1bf9b427ae
tokuD <megumu112851@gmail.com>
2023-11-19 18:08:56 +0900

    fix

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,6 +27,7 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
+                        alt="invoice image"
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
